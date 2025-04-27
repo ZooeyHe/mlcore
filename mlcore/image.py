ce288029f98adfcf589ae1a97551d40afbcf00f3
Zooey He <zhuohonghe@yahoo.com>
2025-04-26 19:39:03 -0700

    added a save_img function which makes it easy to save rgb and grayscale images

--- mlcore/image.py
+++ mlcore/image.py
@@ -5,6 +5,8 @@ date: 2025-04-09
 """
 
 import cv2
+import os
+from PIL import Image
 import numpy as np
 from einops import rearrange
 
@@ -38,3 +40,33 @@ def read_img(filepath: str, out_ein: str = None) -> np.ndarray:
         return rearrange(img_arr, f"H W C -> {out_ein}")
     else:
         return img_arr
+
+
+def save_img(img_arr: np.ndarray, filepath: str, img_ein: str = None) -> None:
+    """
+    Given an image array, save it to file.
+
+    Args:
+        img_arr: a np.ndarray, a numpy array which contains image data.
+        filepath: a save filepath (.png).
+        img_ein: the np.ndarray image's ein. Default is "H W C"
+    """
+    if not os.path.splitext(filepath)[1] in [".png", ".jpg", ".jpeg"]:
+        raise ValueError("Expecting image filepath to be .png")
+    
+    if img_ein:
+        img_arr = rearrange(img_arr, f"{img_ein} -> H W C")
+
+    if img_arr.dtype == np.uint8 and img_arr.max() < 256 and img_arr.min() >= 0:
+        pass  # Already acceptable type.
+    else:
+        # Convert the array values to between 0 - 255
+        img_arr = img_arr.astype(np.float64)
+        img_arr = img_arr / img_arr.max()
+        img_arr = (img_arr * 255).astype(np.uint8)
+
+    img = Image.fromarray(img_arr)
+
+    img.save(filepath)
+
+    
